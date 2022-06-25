c04e8fb37c9f10f0092fab3368717bf82ccb94ef
Livingston-k <kaddulivingston@gmail.com>
2022-06-25 12:51:52 +0300

    change

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,29 @@
 router = require("express").Router()
+const { verifyTokenAndAuthorization } = require('../middleware/middlewareFunctions')
 
-router.get('/users', (req, res) => {
-    res.send("User test is successfull")
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    // VALIDATE
+    if (req.body.username == "") {
+        res.status(424).json({ 'msg': 'Please enter user name ', });
+    }
+    if (req.body.email == "") {
+        res.status(424).json({ 'msg': 'Please enter email address ', });
+    }
+    if (req.body.password == "") {
+        res.status(424).json({ 'msg': 'Please enter password', });
+    }
+    //  HARSH PASSWORD
+    const harshedpassword = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_PASSPHASE).toString()
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true })
+        res.status(200).json(updatedUser)
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
-
-router.post('/save_user', (req, res) => {
-    let name = req.body;
-    console.log(name)
-    // res.send("Your name is " + name);
-})
-
 
 module.exports = router
